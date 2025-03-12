835c18ccf771f0074add8e46014070c657fb4334
Ella-CG <ella23050@yandex.ru>
2025-03-12 03:11:35 +0300

    Домашнее задание-4
    Реализовала добавление элементов по нажатию на Enter. Очистила input после добавления. Добавила возможность зачеркивания элементов.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const sendInput = document.querySelector('#input');
+const itemsContainer = document.querySelector('#items');
+
+sendInput.addEventListener('keydown', function(event) {
+
+
+    const itemText = sendInput.value;
+
+    const newItem = document.createElement('div');
+    newItem.classList.add('items');
+    newItem.addEventListener('click', function() {
+        newItem.classList.toggle('done');
+    });
+    newItem.textContent = itemText;
+
+    if (event.key == 'Enter' && itemText != '') {
+        itemsContainer.append(newItem);
+        sendInput.value = '';
+    };
+
+})
+
+
+
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
